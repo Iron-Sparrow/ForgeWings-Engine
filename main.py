b96c3b066e7eb60b1eca054e03f551a9fc22a661
LittleBread69 <111202841+LittleBread69@users.noreply.github.com>
2023-06-07 21:09:23 +0200

    Small changes

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ from settings import *
 from const import *
 import pygfx as gfx
 from wgpu.gui.auto import WgpuCanvas
-from AntiAliasing import *
 
 canv = WgpuCanvas(title="PyGFX", size=(1280, 720))
 rend = gfx.renderers.WgpuRenderer(canv)
 camera = Camera(80, 1280, 720, 0.1, 2000)
-cam = gfx.PerspectiveCamera(80, camera.width / camera.height, 0.1, 2000) # type: ignore
+cam = gfx.PerspectiveCamera(80, camera.width / camera.height, 0.1, 2000) #type: ignore
 disp = gfx.Display(canvas= canv, renderer= rend, camera= cam)
 #initialising
 scene = gfx.Scene()
@@ -21,3 +20,4 @@ def __main__():
         sleep(c1000)
 
 __main__()
+
