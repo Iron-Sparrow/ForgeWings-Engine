63f3ea6436615a16ae2ef96d897a08a524138e31
LittleBread69 <111202841+LittleBread69@users.noreply.github.com>
2023-08-27 14:14:45 +0200

    Starting over, again.
    Starting over, again.

--- settings.py
+++
@@ -1,9 +0,0 @@
-import pygfx as gfx
-
-class Camera:
-    def __init__(self, fov: int,  width: int, height: int, near: float, far: int):
-        self.fov = fov
-        self.width = width
-        self.height = height
-        self.near = near
-        self.far = far
