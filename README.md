e7781f81c989d88c8e6fa988752b429869a81ae9
LittleBread69 <111202841+LittleBread69@users.noreply.github.com>
2023-09-16 09:37:35 +0200

    Back to ModernGL

--- README.md
+++ README.md
@@ -1,21 +1,30 @@
 # ForgeWings Engine
 
 ## What's ForgeWings ?
-  ForgeWings is the first game engine that is being created from (almost) scratch by IronSparrow. Currently, it is not even sure if it will ever be working as it is still in the planning stage. 
+
+  ForgeWings is the first game engine that is being created from (almost) scratch by IronSparrow. Currently, it is not even sure if it will ever be working as it is still in the planning stage.
 
 ## What's coming in the future ?
+
   There's nothing that is planned but it doesn't mean that the project is dead.
 
 ## What's special about this game engine ?
+
   Well, we would say that not much but some caracteristics would be that it's written purely in Python3 (perhaps in Mojo🔥 later on) and that it changes over time. We'll still try to have some backwards compatibility.
- 
+
 ## What can I do with it ?
+
   Currently there's not much anyone can do, but if anyone wants to contribute to the code, or wants to use it for their own game or even a new game engine, they can. While it's not an obligation, a shoutout or a link to the project would be appreciated.
   Small note before anyone wants to contribute: vandalising, innapropriate behaviour and/or content, spam, etc. is forbidden and we reserve our right to revert any changes and to block any user or contributor.
   With that said, the project is currently under the MIT license but we plan to modify it a bit in the future.
   
-## I want to help !
+## I want to helpxx
 
 ### Make a donation
+
 [![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/V7V0LFW8Y)
+
 ### Contribute to the code
+
+  Currently, contribuing to the code isn't recommended.
+  
--- main.py
+++ main.py
@@ -1,22 +1,73 @@
-import sys
-import sdl2.ext
-import sdl2
+from pprint import pprint
+import moderngl as mgl
+import multiprocessing as mpr
+import glfw
+import time as t
 
-RESSOURCES = sdl2.ext.Resources(__file__, "res")
+# vars
+width, height = 1280, 700
+TUS = 0.0 # TimedUpdate() Speed
+HzTUS = 0.0
 
-sdl2.ext.init()
-window = sdl2.ext.Window("Hello World", size=(960, 600))
-window.show()
+GLFW_CLOSE_WINDOW = False
 
-factory = sdl2.ext.SpriteFactory(sdl2.ext.SOFTWARE)
-sprite = factory.from_image(RESSOURCES.get_path("hello.png"))
+def Update(ctx, window):
+    pprint(ctx.info["GL_VENDOR"])
+    glfw.make_context_current(window)
+    
+    while GLFW_CLOSE_WINDOW == False:
+        ctx.clear(0.0, 0.0, 0.0)
+        #loop
 
-sprite.position = 16, 10
+        glfw.swap_buffers(window)
+        glfw.poll_events()
 
-spriterenderer = factory.create_sprite_render_system(window)
-spriterenderer.render(sprite)
+def TimedUpdate(TUS, HzTUS):
+    if TUS < 1.0 or HzTUS <= 0.0:
+        glfw.terminate()
+        raise ValueError(f"Problem with TUS: {TUS} or with HzTUS: {HzTUS}.")
+    while GLFW_CLOSE_WINDOW == False:
+        st = t.time()
+        #loop
+        
+        if((t.time() - st) < HzTUS):
+            t.sleep(abs(HzTUS - t.time()))
 
-processor = sdl2.ext.TestEventProcessor()
-processor.run(window)
+def Quit(UpdateProcess, TimedUpdateProcess):
+    UpdateProcess.join()
+    TimedUpdateProcess.join()
+    UpdateProcess.terminate()
+    TimedUpdateProcess.terminate()
+    glfw.terminate()
 
-sdl2.ext.quit()
+def GlobalCheck(window):
+    while True:
+        if glfw.window_should_close(window):
+            GLFW_CLOSE_WINDOW.value = True
+              
+if __name__ == '__main__':
+    #Preload Section
+    TUS = 60.0
+    HzTUS = 1.0/TUS
+    
+    glfw.init()
+    if not glfw.init():
+        raise Exception("GLFW initialization failed")
+    
+    window = glfw.create_window(width, height, "ModernGL Window", None, None)
+    if not window:
+        glfw.terminate()
+        raise Exception("Window Creation Failed")
+    with mpr.Manager() as manager:
+        GLFW_CLOSE_WINDOW = manager.Value('b', False)
+        shared_ctx = mgl.create_context(standalone=True, require=410, share=True)
+
+        UpdateProcess = mpr.Process(target=Update, args=(shared_ctx, window, GLFW_CLOSE_WINDOW))
+        TimedUpdateProcess = mpr.Process(target=TimedUpdate, args=(shared_ctx, TUS, HzTUS, GLFW_CLOSE_WINDOW))
+        CheckProcess = mpr.Process(target=GlobalCheck, args=(window, GLFW_CLOSE_WINDOW))
+        # End of Preload Section
+        UpdateProcess.start()
+        TimedUpdateProcess.start()
+        CheckProcess.start()
+       
+        Quit(UpdateProcess, TimedUpdateProcess)
